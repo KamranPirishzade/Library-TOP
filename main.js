903596e72e64977c3d90b87428b155a35a6b5ccb
Kamran <KamranPirishzade@gmail.com>
2025-08-21 23:31:00 -0700

    Adding delete, toggle read button and modal for adding new book

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const myLibrary = [
+let myLibrary = [
   new Book("J.R.R Tolkien", "The Hobbit", 295, false),
   new Book("J.R.R Tolkien", "The Hobbit", 295, false),
   new Book("J.R.R Tolkien", "The Hobbit", 295, false),
 ];
 function Book(author, title, pages, read) {
+  this.id = crypto.randomUUID();
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
 }
 
+let showFormBtn = document.querySelector(".showBtn");
+let modal = document.querySelector("#formModal");
+let overlay = document.querySelector(".overlay");
+let closeBtn = document.querySelector(".close");
 updateLibrary();
 
 function addBookToLibrary(form) {
@@ -24,15 +29,34 @@ function addBookToLibrary(form) {
 }
 
 function updateLibrary() {
+  document.querySelector(".books").replaceChildren();
   myLibrary.forEach((book) => {
     let bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
+    bookDiv.setAttribute("key", book.id);
     bookDiv.innerHTML = `
   <h1>${book.title}</h1>
   <h3>${book.author}</h3>
-  <p>Pages: ${book.pages}</p>
   <p>${book.read ? "Already read" : "Not read yet"}</p>
+  <p>Pages: ${book.pages}</p>
   `;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", () => {
+      myLibrary = myLibrary.filter((bookItem) => bookItem.id !== book.id);
+      updateLibrary();
+    });
+    deleteBtn.classList.add("delete");
+    bookDiv.appendChild(deleteBtn);
+    const toggleRead = document.createElement("button");
+    toggleRead.textContent = book.read ? "Not read yet" : "I read it" ;
+    toggleRead.addEventListener("click", (e) => {
+      book.read = !book.read;
+      updateLibrary();
+    });
+    toggleRead.classList.add("toggle");
+    bookDiv.appendChild(deleteBtn);
+    bookDiv.appendChild(toggleRead);
     document.querySelector(".books").appendChild(bookDiv);
   });
 }
@@ -40,7 +64,20 @@ function updateLibrary() {
 let form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  document.querySelector(".books").innerHTML = "";
   addBookToLibrary(form);
   updateLibrary();
+  modal.close();
+  overlay.style.display = "none";
+  e.target.reset();
+});
+
+showFormBtn.addEventListener("click", () => {
+  modal.showModal();
+  overlay.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+  overlay.style.display = "none";
+  form.reset();
 });
